30f14e2e9dde8b33378c5bc07dede3fecb412dd2
mynguyenky <supertom309@gmail.com>
2023-05-29 08:51:42 +0700

    server

---
+++ demo_module.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var dt = require('./mymodule');
+
+exports.myDateTime = function () {
+	return Date();
+};
+
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("The date and time are currently: " + dt.myDateTime());
+	res.end();
+}).listen(8080);
